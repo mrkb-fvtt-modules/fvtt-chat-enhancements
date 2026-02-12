614924d48f7f63a2738c213f92d0f793a7d7a2ea
MRKB <asdfgh7913@naver.com>
2026-02-12 19:52:01 +0900

    fix: disable debug hooks in constants configuration

--- scripts/constants.mjs
+++ scripts/constants.mjs
@@ -3,6 +3,6 @@ const MODULE_PATH = `modules/${MODULE_ID}`;
 const ASSETS_PATH = `${MODULE_PATH}/assets`;
 const ALTER_IMAGE = `${ASSETS_PATH}/images/softcolor.jpg`;
 const TYPING_PLAYERS = [];
-CONFIG.debug.hooks = true;
+//CONFIG.debug.hooks = true;
 
 export { MODULE_ID, MODULE_PATH, ASSETS_PATH, ALTER_IMAGE };
